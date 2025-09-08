bd38eba43519e3a32b00263ba2e8214488837fd8
unknown <aqibikmal10@gmail.com>
2025-09-08 15:02:43 +0800

    Modernize header, footer, and contact page design

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // Import RouterLink
+import { ScrollFadeInDirective } from '../../directives/scroll-fade-in'; // Import Directive
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,              // Tambah di sini
+    ScrollFadeInDirective  // Tambah di sini
+  ],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class HomeComponent {
+
 }
